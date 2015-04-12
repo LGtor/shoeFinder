7312caf695a39315a1ea82de34be77ff9a0ceda5
Logan <Logan@alcatrazs-Mac-Pro.local>
2015-04-11 20:11:20 -0400

    added app.js + angular controlelrs & front end markup (to be styled)

---
+++ README.md
@@ -0,0 +1,65 @@
+# ShoeFinder App
+
+An Angular JS + Foundation 5 running shoe generator app
+
+
+
+Additions to Foundation's default package include:
+
+* Stylesheets have been broken up into multiple files. One for small, medium and large. As well as a settings file and mixins file.
+* A default Javascript class has been added to app.js.
+* A small debug element has been added to the bottom left corner to indicate screen width and current stylesheet in use.
+* Support for IE8 has been added, thanks to James Cocker. (http://foundation.zurb.com/forum/posts/241-foundation-5-and-ie8)
+
+## Requirements
+
+  * Ruby 1.9+
+  * [Node.js](http://nodejs.org)
+  * [compass](http://compass-style.org/): `gem install compass`
+  * [bower](http://bower.io): `npm install bower -g`
+  * [angular](http://angluar.com): `npm install bower -g`
+
+## Quickstart
+
+1. Change into the /library/ directory.
+2. Type 'compass watch', press enter.
+
+## Thanks
+
+1. Mike Turner @ GoCactus (www.gocactus.com) - foundation boilerPlate
+2. Wisam Zaghal @ rangle.io (http://rangle.io) 
+3. James Cocker - IE8 compatibility
+
+-------------------------------------------
+
+# Foundation Compass Template (Original Foundation README)
+
+The easiest way to get started with Foundation + Compass.
+
+## Requirements
+
+  * Ruby 1.9+
+  * [Node.js](http://nodejs.org)
+  * [compass](http://compass-style.org/): `gem install compass`
+  * [bower](http://bower.io): `npm install bower -g`
+  * [angular JS](https://angularjs.org/): `bower install angular`
+
+
+## Quickstart
+
+  * [Download this starter compass project and unzip it](https://github.com/zurb/foundation-compass-template/archive/master.zip)
+  * Run `bower install` to install the latest version of Foundation
+
+Then when you're working on your project, just run the following command:
+
+```bash
+bundle exec compass watch
+```
+
+## Upgrading
+
+If you'd like to upgrade to a newer version of Foundation down the road just run:
+
+```bash
+bower update
+```
---
+++ html/library/js/app.js
@@ -0,0 +1,160 @@
+/*jslint browser: true */
+/*global $, jQuery, alert, console, Foundation5Boilerplate:true */
+
+var console = console || { log: function() { 'use strict'; } };
+
+
+
+window.shoeFinder = window.shoeFinder || {};
+
+(function (s) {
+
+    'use strict';
+
+    s.App = function () {
+
+        return {
+            initialized: false,
+            elements: {},
+            settings: {
+                debug: false,
+                host_url: 'http://' + window.location.hostname + '/'
+            },
+
+            init: function () {
+
+
+                if (this.settings.debug) { console.log('init()'); }
+
+                if (this.initalized) { return false; }
+                this.initialized = true;
+
+                //dom elements
+                this.elements.body =  $('body', 'html');
+                this.elements.debug = $('#txtDebug', this.elements.body);
+                // this.elements.currentYear = $('.current-year', this.elements.footer);
+
+                //configure debug based on config file
+                if (this.elements.debug.val()) {
+                    this.settings.debug = true;
+                    this.initDebug();
+                }
+
+                //initialize foundation
+                $(document).foundation();
+
+
+            },
+
+       
+            initDebug: function () {
+
+                if (this.settings.debug) { console.log('initDebug()'); }
+
+                $(this.elements.body).append('<div id="debug-message"></div>');
+                $('#debug-message').append('<p class="small">small</p><p class="medium">medium</p><p class="large">large</p><p class="exlarge">extra large</p>');
+                $(window).resize(function () {
+                    $('#debug-message').empty();
+                    $('#debug-message').append('<p class="small">small</p><p class="medium">medium</p><p class="large">large</p><p class="exlarge">extra large</p>');
+                    $('#debug-message').append('<p>width: ' + window.innerWidth + '</p>');
+                });
+
+            }
+
+        };
+
+    };
+
+}(shoeFinder));
+
+
+
+$(document).ready(function() {
+    'use strict';
+    shoeFinder.App().init();
+});
+
+/* ==========================================================================
+    START ANGULAR APP
+   ========================================================================== */
+
+var app = angular.module('myApp', []);
+
+
+app.controller('mainController', ['$scope', function($scope) {
+  
+    $scope.selectedShoes = [];
+
+
+
+
+
+
+    $scope.process = function() {
+        
+        $scope.selectedShoes = [];
+       
+        var userSelected = $scope.userSelected;
+
+        for (var i = 0; i < $scope.shoes.length; i++) {
+            if ($scope.shoes[i].brand === userSelected) {
+                $scope.selectedShoes.push($scope.shoes[i]);
+            }
+        }
+
+    };
+
+    $scope.shoes = [{
+            brand: "Nike",
+            gender: "male",
+            model: "Free 5.0",
+            color: "black",
+            activity: "running",
+            price: 129.99,
+            image: "../html5-boilerplate/img/nikeFree5.0.jpg"
+        },
+
+        {
+            brand: "Nike",
+            gender: "male",
+            model: "Flyknit 4.0",
+            color: "black",
+            activity: "running",
+            price: 159.99,
+            image: "../html5-boilerplate/img/nikeFlyknit.jpg"
+        },
+
+
+
+        {
+            brand: "Reebok",
+            gender: "male",
+            model: "Z Pump Fusion ",
+            color: "black /yellow",
+            activity: "running",
+            price: 139.99,
+            image: "../html5-boilerplate/img/reebokZPump.jpg"
+        },
+
+        {
+            brand: "Reebok",
+            gender: "female",
+            model: "Z aPump Fusion ",
+            color: "black f/yellow",
+            activity: "frunning",
+            price: 149.99,
+            image: "../html5-boilerplate/img/reebokZPump.jpg"
+        }
+
+
+
+
+    ]
+
+
+
+}]);
+
+
+
+
